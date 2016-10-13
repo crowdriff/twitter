f8cd104ba00c77803ce193d698c61489c91e6369
ryanfowler <ryan.fowler19@gmail.com>
2016-10-13 19:39:37 -0400

    Add destroy tweet endpoint

--- twitter.go
+++ twitter.go
@@ -12,6 +12,12 @@ type DirectMessagesResponse struct {
 	RateLimit      RateLimit
 }
 
+// TweetResponse represents a response from Twitter containing a single Tweet.
+type TweetResponse struct {
+	Tweet     Tweet
+	RateLimit RateLimit
+}
+
 // TweetsResponse represents a response from Twitter containing multiple Tweets.
 type TweetsResponse struct {
 	Tweets    []Tweet
